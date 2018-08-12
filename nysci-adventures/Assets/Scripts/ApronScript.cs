8ab1f92970ecaa87f2e9bf893ac6a9a98f55ff7f
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-08-12 14:47:06 -0400

    8/12/2018
    Starting on desktop

---
+++ nysci-adventures/Assets/Scripts/ApronScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApronScript : MonoBehaviour {
+
+	public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); 
+    }
+}
---
+++ nysci-adventures/Assets/Scripts/DataController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+
+public class DataController : MonoBehaviour
+{
+    public RoundData[] allRoundData;
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+       
+
+        SceneManager.LoadScene("MapScreen");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+ 
+
+}
+
+  
